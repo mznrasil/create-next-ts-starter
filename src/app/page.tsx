080090f12c468c53120530e9f17000eb926282ab
mznrasil <mzn.rasil@gmail.com>
2024-01-05 11:07:29 +0545

    feat: shadcn configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <main className='flex min-h-screen items-center justify-center text-2xl'>
       Happy Coding 🙌
+      <Button>Button</Button>
     </main>
   );
 }
